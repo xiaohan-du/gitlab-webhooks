12adb6e420ba67884c48fb84e746c263bf5d97e5
Xiaohan Du <dux18@cardiff.ac.uk>
2023-06-29 10:33:03 +0100

    Initial commit

---
+++ src/main/java/com/example/gitlabwebhooks/GitlabWebhooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabwebhooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabWebhooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabWebhooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitlabwebhooks/GitlabWebhooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlabwebhooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabWebhooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
