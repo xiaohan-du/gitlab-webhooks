a64c0d8b446c3840cdb81ff63a4bd184f03c4fff
Xiaohan Du <dux18@cardiff.ac.uk>
2023-07-15 22:15:11 +0000

    Fetch api data to fe

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,42 @@
+'use client'
+
 import Image from 'next/image'
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [testData, setTestData] = useState('');
+  async function getData() {
+    const res = 'http://localhost:8080/api/webhook';
+    let req = Object.assign(
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        }
+    );
+
+    try {
+      const response = await fetch(res, req);
+      const data = await response.json();
+      const [unwrappedData] = data;
+      setTestData(JSON.stringify(unwrappedData));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [getData()]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          <code className="font-mono font-bold">data {testData}</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
